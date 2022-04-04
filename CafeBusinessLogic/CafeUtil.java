d656c61b803a80feb4befb719700a4f59893841f
LaurenOdalen <laurenodalen@gmail.com>
2022-04-04 07:15:54 -0600

    commit

---
+++ CafeBusinessLogic/CafeUtil.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+
+
+public class CafeUtil {
+    
+int getStreakGoal(numWeeks);
+
+ArrayList<Integer> myTotal = new ArrayList<Integer>();
+return myTotal;
+}
+
+
+// double getOrderTotal(double[] prices)
+
+
+
+// void displayMenu(ArrayList<String> menuItems)
+
+
+// addCustomer(ArrayList<String> customers)
+// String userName = System.console().readLine();
+
+
+}
